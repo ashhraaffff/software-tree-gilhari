beaddc87d62fd15b40aebd92d78e6c8b57c5eb7e
ashhraaffff <ashraf1172004@gmail.com>
2024-07-10 20:37:59 +0530

    working code

--- src/BookstoreSchema/curlCommands.sh
+++ src/BookstoreSchema/curlCommands.sh
@@ -15,40 +15,40 @@
 echo -e "** BEGIN OUTPUT **" > curl.log
 
 echo "** Delete all Bookobjects to start fresh" >> curl.log
-curl -X DELETE "http://localhost:80/gilhari/v1/Bookstore" >> curl.log
+curl -X DELETE "http://localhost:80/gilhari/v1/Book" >> curl.log
 echo "" >> curl.log
 
 echo -e "** Insert one Bookobject \n" >> curl.log
-curl -X POST "http://localhost:80/gilhari/v1/Bookstore"  -H 'Content-Type: application/json' -d '{"entity":{"bookid":1,"bookname":"Hobbit","author":"Tolkien","description":"The story of hobbit","price":300,"release_date":381484800390}}' >> curl.log
+curl -X POST "http://localhost:80/gilhari/v1/Book"  -H 'Content-Type: application/json' -d '{"entity":{"bookid":1,"bookname":"Hobbit","author":"Tolkien","description":"The story of hobbit","price":300,"release_date":381484800390}}' >> curl.log
 echo -e "" >> curl.log
 
 echo "** Query all Bookobjects" >> curl.log
-curl -X GET   "http://localhost:80/gilhari/v1/Bookstore"  -H 'Content-Type: application/json' >> curl.log
+curl -X GET   "http://localhost:80/gilhari/v1/Book"  -H 'Content-Type: application/json' >> curl.log
 echo "" >> curl.log
 
 echo "** Insert multiple (two) Bookobjects" >> curl.log
-curl -X POST "http://localhost:80/gilhari/v1/Bookstore"  -H 'Content-Type: application/json' -d '{"entity":{"bookid":2,"bookname":"Hobbit2","author":"Tolkien2","description":"The story of hobbit2","price":400,"release_date":381484890390}}' >> curl.log
+curl -X POST "http://localhost:80/gilhari/v1/Book"  -H 'Content-Type: application/json' -d '{"entity":{"bookid":2,"bookname":"Hobbit2","author":"Tolkien2","description":"The story of hobbit2","price":400,"release_date":381484890390}}' >> curl.log
 echo "" >> curl.log
 
 echo "** Query all Bookobjects" >> curl.log
-curl -X GET   "http://localhost:80/gilhari/v1/Bookstore"  -H 'Content-Type: application/json' >> curl.log
+curl -X GET   "http://localhost:80/gilhari/v1/Book"  -H 'Content-Type: application/json' >> curl.log
 echo "" >> curl.log
 
 echo "** Query non-exempted Bookobjects" >> curl.log
-curl -X GET   "http://localhost:80/gilhari/v1/Bookstore?filter=exempt=0"  -H 'Content-Type: application/json' >> curl.log
+curl -X GET   "http://localhost:80/gilhari/v1/Book?filter=exempt=0"  -H 'Content-Type: application/json' >> curl.log
 echo "" >> curl.log
 
 echo "** Query the count of exempted Bookobjects" >> curl.log
-curl -X GET   "http://localhost:80/gilhari/v1/Bookstore/getAggregate?attribute=id&aggregateType=COUNT&filter=exempt=1"  -H 'Content-Type: application/json' >> curl.log
+curl -X GET   "http://localhost:80/gilhari/v1/Book/getAggregate?attribute=id&aggregateType=COUNT&filter=exempt=1"  -H 'Content-Type: application/json' >> curl.log
 echo "" >> curl.log
 
 
 echo "** Query the count of all exempted Bookobjects" >> curl.log
-curl -X GET   "http://localhost:80/gilhari/v1/Bookstore/getAggregate?attribute=id&aggregateType=COUNT&filter=exempt=1"  -H 'Content-Type: application/json' >> curl.log
+curl -X GET   "http://localhost:80/gilhari/v1/Book/getAggregate?attribute=id&aggregateType=COUNT&filter=exempt=1"  -H 'Content-Type: application/json' >> curl.log
 echo "" >> curl.log
 
 echo "** Query the count of all Bookobjects" >> curl.log
-curl -X GET   "http://localhost:80/gilhari/v1/Bookstore/getAggregate?attribute=id&aggregateType=COUNT"  -H 'Content-Type: application/json' >> curl.log
+curl -X GET   "http://localhost:80/gilhari/v1/Book/getAggregate?attribute=id&aggregateType=COUNT"  -H 'Content-Type: application/json' >> curl.log
 echo "" >> curl.log
 
 echo "** END OUTPUT **" >> curl.log
